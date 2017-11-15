480bddf9bada343cac7b553f7d29fc8180cf1889
snyambura <snyambura94@gmail.com>
2017-11-15 08:30:02 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+/* File: gulpfile.js */
+
+// grab our gulp packages
+var gulp  = require('gulp');
+
+var Server = require('karma').Server;
+
+
+gulp.task('test', function (done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
+});
+
+gulp.task('re-test', function (done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js'
+  }, done).start();
+});
+
+gulp.task('default', ['re-test']);
